c6465383430d3b05fb5db46d1c2a9c05700ebd4b
github username <github email address>
2020-03-12 16:06:42 -0400

    fetched player data and mapped to use on showpage

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,13 @@ User.destroy_all
 goal_key = Rails.application.credentials.goooal[:api_key]
 goal_response = RestClient.get("http://api.football-data.org/v2/competitions/2021/teams", {"X-Auth-Token" => "#{goal_key}"})
 response = JSON.parse(goal_response)
-
 teams_array = response["teams"]
 
+
+players_response = RestClient.get("http://api.football-data.org/v2/teams/66",{"X-Auth-Token" => "#{goal_key}"})
+p_res = JSON.parse(players_response)
+player_array = p_res["squad"]
+# byebug
 
 teams_array.each do |team|
 
@@ -29,6 +33,17 @@ club_colors = team['clubColors']
 venue = team['venue']
 
 Team.create(full_name: full_name, short_name: short_name, tla: tla_name, crestUrl: crestUrl, address: address, phone: phone, website: website, email: email, founded: founded, club_colors: club_colors, venue: venue)
+end
+
+player_array.each do |player|
+name = player["name"]
+position = player["position"]
+date_of_birth = player["dateOfBirth"]
+nationality = player["nationality"]
+shirt_number = player["shirtNumber"]
+role = player["role"]
+
+Player.create(name: name, position: position, date_of_birth: date_of_birth, nationality: nationality, shirt_number: shirt_number, role: role)
 end
 
 # byebug
